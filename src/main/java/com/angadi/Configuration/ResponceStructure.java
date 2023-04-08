1b4805604999bfe10e10e8ee6733ade5a09f5927
rajumb0232 <rajugowda0232@gmail.com>
2023-04-08 13:57:40 +0530

    added responseStructure

---
+++ src/main/java/com/angadi/Configuration/ResponceStructure.java
@@ -0,0 +1,28 @@
+package com.angadi.Configuration;
+
+public class ResponceStructure<T> {
+	
+	private int status;
+	private String message;
+	private Object data;
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+	
+	
+}
